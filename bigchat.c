d3fc6692a8624b76f4779189a2952d5476de4357
Marry <137852107+gomeo01@users.noreply.github.com>
2023-11-14 14:59:53 +0700

    Update bigchat.c

--- bigchat.c
+++ bigchat.c
@@ -1,8 +1,8 @@
 /* smallchat.c -- Read clients input, send to all the other connected clients.
  *
  * Copyright (c) 2023, Salvatore Sanfilippo <antirez at gmail dot com>
- * All rights reserved.
- *
+ * All rights reserved...
+ **************************
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
